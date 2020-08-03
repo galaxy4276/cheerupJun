1c27d29d6d8bd3099f0ffc36e4fa82919f5d21bb
bearbearbear <ghlwkdthfl@gmail.com>
2020-08-04 02:48:20 +0900

    Meaningless change

--- build/app.js
+++ build/app.js
@@ -3,14 +3,9 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import path from 'path';
 import router from './routes/testRouter';
+
 const app = express();
-<<<<<<< HEAD
 const host = '10.178.0.2';
-=======
-
-
-const host = '10.173.0.2';
->>>>>>> origin/backend
 const port = 8001;
 
 app.set('view engine', 'pug');
