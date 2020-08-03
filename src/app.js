2ee61e40fa08132e09658fc12626891891f15a70
galaxy4276 <galaxyhi4276@gmail.com>
2020-08-04 05:19:26 +0900

    데이터베이스 연동 / app.js 수정

--- src/app.js
+++ src/app.js
@@ -1,23 +1,53 @@
+// Import Server Modules
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import path from 'path';
+import cookie from 'cookie-parser';
+// import session from 'express-session';
+
+
+// Import Database(Mariadb) Modules
+import { sequelize } from './models'
+
+
+// 사용자 지정 환경변수를 사용할 수 있게 불러옵니다.
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+// Import Router 
 import router from './routes/testRouter';
-import { sharePug } from './middlewares/middlewares';
+
 
 const app = express();
+sequelize.sync()
+  .then(() => {
+    console.log('MariaDB connected');
+  })
+  .catch(err => {
+    console.log('connect fail');
+    console.error(err);
+  });
+
 
 app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 8001);
 app.set('views', path.resolve(__dirname, 'views'));
+
 
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(cookie(process.env.COOKIE_SECRET));
+// app.use(session({
+//   resave: false,
+//   saveUninitialized: false,
+//   secret: process.env.COOKIE_SECRET,
+// }));
 
-app.use(sharePug);
 
 app.use('/', router);
 
--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -1,7 +1,11 @@
 import urls from '../routes/urls';
+import { sequelize } from '../models';
 
 export const sharePug = (req, res, next) => {
   res.locals.pugVariables = urls;
   next();
 };
 
+export const verifyConnectingMariaDB = () => {
+  
+}
